7e0b5f31b50f4ed7832eaea9710fd47bd7a85475
Dragoi Laura <mihaescu.laura@gmail.com>
2021-05-25 07:59:05 +0300

    modified product page tests

--- tests/altex/ProductPageCest.php
+++ tests/altex/ProductPageCest.php
@@ -13,6 +13,7 @@ class ProductPageCest
         $I->seeElement('div.MainMenu.static  ul.MainMenu-list.relative');
         $I->seeElement('.MainMenu-trigger--products');
         $I->clickWithLeftButton('.MainMenu-trigger--products');
+        $I->wait(1);
         $I->seeElement('.MainMenu-submenu.ProductsMenu');
         $I->seeLink('Promotii','https://altex.ro/promo/');
         $I->seeLink('Branduri','https://altex.ro/branduri/');
@@ -35,8 +36,49 @@ class ProductPageCest
     }
 
     public function testingProductImageElements(AltexTester $I){
-
+        $I->seeElement('.u-heading-uppercase');
+        $I->seeElement('.mt-2 .font-bold');
+        $I->seeElement('.gallery-container');
+        $I->seeElement('.product-shop div.Price-current:first-child');
+        $I->seeElement('.product-shop .Status');
+        $I->seeElement('.mb-3 button.bg-primary');
+        $I->seeElement('.mb-3 button.bg-white');
+        $I->seeElement('div.AddToButtons .flex-1:nth-child(1)');
+        $I->seeElement('div.AddToButtons div.flex-1:last-of-type');
+        $I->seeElement('#description');
+        $I->seeElement('#additional');
+        $I->seeElement('#reviews');
+        $I->seeElement('#questions');
 
     }
+
+    public function testOtherVisitorsAlsoBought(AltexTester $I)
+    {
+        $I->scrollTo('.my-12 + div');
+        $I->seeElement('.my-12 + div h3');
+        $I->seeElement('.my-12 + div .border.rounded');
+        $I->seeElement('.my-12 + div .mb-3');
+        $I->seeElement('.my-12 + div .Status--inStock');
+        $I->seeElement('.my-12 + div .text-sm ');
+        $I->seeElement('.my-12 + div .Price--discount');
+        $I->seeElement('.my-12 + div .mt-2');
+    }
+
+   // public function testSeenRecently(AltexTester $I)
+   // {
+
+       // $I->clickWithLeftButton('.my-12 + div .mx-auto.mb-3');
+        //$I->wait(3);
+       // $I->amOnUrl('https://altex.ro/sapun-dove-cream-oil-100g/cpd/CRM9167012/');
+     //   $I->wait(2);
+
+      //  $I->scrollTo('.my-12 + div + div');
+
+       // $I->seeElement('.my-12 + div + div h3');
+       // $I->seeElement('.my-12 + div + div .mb-2');
+      //  $I->seeElement('.my-12 + div + div .mb-3');
+       // $I->seeElement('.my-12 + div + div h2');
+
+   // }
 }
 
