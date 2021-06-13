979e0a48164bb90c52dc899796bba2db5e7a40ef
Dragoi Laura <mihaescu.laura@gmail.com>
2021-06-13 11:53:26 +0300

    :q partially finished product list page tests

--- tests/altex/ProductListPageCest.php
+++ tests/altex/ProductListPageCest.php
@@ -17,10 +17,6 @@ class ProductListPageCest
         $I->seeElement('#catalog-filters-container');
         $I->seeLink('Promotii','https://altex.ro/laptopuri/cpl/filtru/promotii-3803/promotii/');
         $I->seeLink('In stoc','https://altex.ro/laptopuri/cpl/filtru/in-stoc-5182/in-stoc/');
-        $I->seeLink('Resigilate','https://altex.ro/laptopuri/cpl/filtru/resigilate-8691/yes/');
-        $I->seeLink('In stoc furnizor','https://altex.ro/laptopuri/cpl/filtru/in-stoc-furnizor-6635/yes/');
-        $I->seeLink('Noutati','https://altex.ro/laptopuri/cpl/filtru/noutati-5218/noutati/');
-        $I->seeLink('Precomanda','https://altex.ro/laptopuri/cpl/filtru/precomanda-5217/precomanda/');
         $I->see('Pret','.Filters-name');
         $I->see('Brand','.Filters-name');
         $I->see('Categorie','.Filters-name');
@@ -52,4 +48,41 @@ class ProductListPageCest
         $I->seeLink('Sterge tot','https://altex.ro/laptopuri/cpl/');
         $I->seeLink('General: Promotii','https://altex.ro/laptopuri/cpl/');
     }
+
+    public function testThePresenceOfSortingTab(AltexTester $I)
+    {
+        $I->seeElement('div.ulg-u-display-none');
+        $I->seeElement('//body/div[@id=\'__next\']/div[@class=\'u-container\']/main[@class=\'lg-u-space-pb-20\']/div[@class=\'u-clearfix\']/div[contains(@class,\'lg-u-float-right lg-u-size-8of10\')]/div/div[@class=\'u-container-reset u-border-t-solid\']/div[@class=\'Toolbar u-clearfix\']/div[@class=\'u-display-iblock ulg-u-display-none\']/div/div[@class=\'u-display-iblock\']/div[1]/div[1]/select[1]');
+        $I->see('Sorteaza dupa','.u-display-iblock');
+        $I->see('Vezi','.u-display-iblock');
+        $I->see('Arata','.u-display-iblock');
+        $I->see('Pagina','.u-display-iblock');
+    }
+
+    public function testTheFunctionalityOfSortingTab(AltexTester $I)
+    {
+        $bestSoldFirstProductPrice = $I->grabTextFrom('.Products-item:first-child span.Price-int');
+
+        $I->selectOption('.js-trigger-catalog-toolbar-apply-filters', array('text' => 'Pret crescator'));
+        $I->wait(2);
+        $I->dontSee($bestSoldFirstProductPrice,'.Products-item:first-child span.Price-int');
+        $I->seeInCurrentUrl('asc');
+
+        $I->selectOption('.js-trigger-catalog-toolbar-apply-filters', array('text' => 'Pret descrescator'));
+        $I->wait(2);
+        $I->dontSee($bestSoldFirstProductPrice,'.Products-item:first-child span.Price-int');
+        $I->seeInCurrentUrl('desc');
+
+        $I->selectOption('.js-trigger-catalog-toolbar-apply-filters', array('text' => 'Nume'));
+        $I->wait(2);
+        $I->dontSee($bestSoldFirstProductPrice,'.Products-item:first-child span.Price-int');
+        $I->seeInCurrentUrl('/name/');
+
+        $I->selectOption('.js-trigger-catalog-toolbar-apply-filters', array('text' => 'Cel mai mare discount'));
+        $I->wait(2);
+        $I->dontSee($bestSoldFirstProductPrice,'.Products-item:first-child span.Price-int');
+        $I->seeInCurrentUrl('/best_deal/');
+
+    }
+
 }
